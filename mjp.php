8e818636bec6c27ccfdc18ac18a84c8319400581
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-10-27 11:15:30 +0000

    initial commit

---
+++ mjp.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martapleszynska
+ * Date: 27/10/15
+ * Time: 11:07
+ */
+// function to filter and remove any character that are not number; allow + sign
