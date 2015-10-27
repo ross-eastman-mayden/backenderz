35bd801113710d58104d0d17adb4e7e5b52e848d
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-10-27 14:07:57 +0000

    added validation funtion

--- mjp.php
+++ mjp.php
@@ -1,35 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: martapleszynska
- * Date: 27/10/15
- * Time: 11:07
- */
+
 // function to filter and remove any character that are not digit; allow + sign
 
 //Ross's code
-public function filterInput ($input) {
+
+$input = "nljheagh2364=+0";
+
+function filterInput ($input) {
     //take input
     //remove anything that is not a digit
     //ignore +
     //return a result
-    return preg_replace('/[^0-9\+]/', NULL, $input);
+    $text = $input;
+    return preg_replace('/[^0-9]/', NULL, $text);
 }
 echo filterInput("nkvd89234hgwng+");
 
-//Connor's code
-function removeNonNumerics ($text){
-    $new_text = $text;
-    $length = strlen($new_text);
-    for ($i = 0; $i < $length; $i++){
-        $char = substr($new_text, $i, 1);
-        $ascii = ord($char);
-        if ((($ascii >= ord('0') && ($ascii <= ord('9'))) || ($ascii == ord('+')))){
-            $new_text[$i] = $char;
-        }
-        else $new_text[$i] = '';
+function validateTelNo ($number){
+    //true begin 07 11 digit long
+    $number = filterInput($string);
+    if ($number[0] == 0 && $number[1] == 7 && strlen($number) == 11){
+        echo $number;
     }
-    return $new_text;
+    else {
+        echo "Your number didnot pass validation!";
+    }
 }
-$test_string = "23412i+uegd+hsi+qw u124321e+kjsdb+klajsh";
-echo removeNonNumerics($test_string);
+echo validateTelNo($input);
+
+//Connor's code
+//function removeNonNumerics ($text){
+//    $new_text = $text;
+//    $length = strlen($new_text);
+//    for ($i = 0; $i < $length; $i++){
+//        $char = substr($new_text, $i, 1);
+//        $ascii = ord($char);
+//        if ((($ascii >= ord('0') && ($ascii <= ord('9'))) || ($ascii == ord('+')))){
+//            $new_text[$i] = $char;
+//        }
+//        else $new_text[$i] = '';
+//    }
+//    return $new_text;
+//}
+//$test_string = "23412i+uegd+hsi+qw u124321e+kjsdb+klajsh";
+//echo removeNonNumerics($test_string);
