8fd22acbe10f01f0faaa0afd1e1dac8a8e3ed53f
charliecog <charlie.coggans@mayden.co.uk>
2015-10-27 14:24:21 +0000

    ended all wars

--- tues.php
+++ tues.php
@@ -1,28 +1,29 @@
 <?php
 
 
-function numericsOnly($startingWord) {
+$startingWord = "079008675d64";
 
 $array = (str_split($startingWord));
 
 $newWord = array();
 
-foreach ($array as &$value) {
+    foreach ($array as &$value) {
 
-    if((ord($value)) <= ord("9") && (ord($value)) >= ord("0") || (ord($value)) == ord("+") ) {
-        array_push($newWord, $value);
+        if((ord($value)) <= ord("9") && (ord($value)) >= ord("0") || (ord($value)) == ord("+") ) {
+            array_push($newWord, $value);
+        }
     }
-}
 
 $implodedWord = implode(" ",$newWord);
 
 $finishedWord = str_replace(' ', '', $implodedWord);
 
-echo $finishedWord;
-
-}
-
-numericsOnly("23kjwlejkljsd-=asf;+lkjdsflj34a");
+    if (strlen($finishedWord) == 11 && (substr($finishedWord, 0, 2)) == '07' ) {
+        echo "Phone number validated: " . $finishedWord;
+    }
+    else {
+        echo "you shall not pass!";
+    }
 
 ?>
 
