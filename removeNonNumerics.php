4686626da2a457e395364fd0d37469fb4afdfc91
connorjones17 <connor.jones@mayden.co.uk>
2015-10-27 13:33:52 +0000

    shortened function, logic unchanged

--- removeNonNumerics.php
+++ removeNonNumerics.php
@@ -1,16 +1,14 @@
 <?php
 
-function removeNonNumerics ($text){
-    $length = strlen($text);
-    for ($i = 0; $i < $length; $i++){
-        $char = substr($text, $i, 1);
-        $ascii = ord($char);
+function removeNonNumerics ($text)
+{
+    for ($i = 0; $i < strlen($text); $i++) {
+        $ascii = ord(substr($text, $i, 1));
         if (!(($ascii >= ord('0') && $ascii <= ord('9')) || $ascii == ord('+'))) {
             $text[$i] = '';
         }
     }
     return $text;
-
 }
 
 $test_string = "1234 1 2i+ uegd,+ hsi+qw !@.<£$a'd'f%^&*(xcv)_*-+*/=*sdfb-+u124321 e+kjsd b+ kla j sh";
