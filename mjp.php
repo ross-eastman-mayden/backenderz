51d724cd3be2813058cb5bc38fd6933cf2e2bbd8
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-10-27 12:37:06 +0000

    failed to write something on my own

--- mjp.php
+++ mjp.php
@@ -5,4 +5,31 @@
  * Date: 27/10/15
  * Time: 11:07
  */
-// function to filter and remove any character that are not number; allow + sign
+// function to filter and remove any character that are not digit; allow + sign
+
+//Ross's code
+public function filterInput ($input) {
+    //take input
+    //remove anything that is not a digit
+    //ignore +
+    //return a result
+    return preg_replace('/[^0-9\+]/', NULL, $input);
+}
+echo filterInput("nkvd89234hgwng+");
+
+//Connor's code
+function removeNonNumerics ($text){
+    $new_text = $text;
+    $length = strlen($new_text);
+    for ($i = 0; $i < $length; $i++){
+        $char = substr($new_text, $i, 1);
+        $ascii = ord($char);
+        if ((($ascii >= ord('0') && ($ascii <= ord('9'))) || ($ascii == ord('+')))){
+            $new_text[$i] = $char;
+        }
+        else $new_text[$i] = '';
+    }
+    return $new_text;
+}
+$test_string = "23412i+uegd+hsi+qw u124321e+kjsdb+klajsh";
+echo removeNonNumerics($test_string);
