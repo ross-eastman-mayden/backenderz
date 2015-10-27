db6b7393ee1ab54d050d519d9afe92d0be6f7f84
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-10-27 15:33:47 +0000

    added interface and class

--- mjp.php
+++ mjp.php
@@ -3,33 +3,56 @@
 // function to filter and remove any character that are not digit; allow + sign
 
 //Ross's code
+interface Filter
+{
+    public function filter($input);
+}
 
-function filterInput ($input) {
-    //take input
-    //remove anything that is not a digit
-    //ignore +
-    //return a result
-    $text = $input;
-    return preg_replace('/[^0-9]/', NULL, $text);
-}
-function validateTelNo ($input){
-    //true begin 07 11 digit long
-    $number = filterInput($input);
-    if ($number[0] == 0 && $number[1] == 7 && strlen($number) == 11){
-        echo $number;
-        $result = true;
+class PhoneFilter implements Filter
+{
+    function filter($input)
+    {
+        //take input
+        //remove anything that is not a digit
+        //ignore +
+        //return a result
+        $text = $input;
+        return preg_replace('/[^0-9]/', NULL, $text);
     }
-    else {
-        echo "Your number did not pass validation!";
+}
+
+interface Validator
+{
+    public function validate($number);
+}
+
+class PhoneValidator implements Validator
+{
+    function validate($number)
+    {
+        //true begin 07 11 digit long
         $result = false;
+        if ($number[0] == 0 && $number[1] == 7 && strlen($number) == 11) {
+            $result = true;
+        }
+        return $result;
     }
-    return $result;
 }
-echo validateTelNo("07861828475"); // valid input
+
+function printResult($number)
+{
+    if (validateTelNo()) {
+        echo $number;
+    } else {
+        echo "Your number did not pass validation!";
+    }
+}
+
+echo validate("07861828475"); // valid input
 echo "<br>";
-echo validateTelNo("hashgro;idshbh0684+04+0"); // invalid input
+echo validate("hashgro;idshbh0684+04+0"); // invalid input
 echo "<br>";
-echo validateTelNo("has0hg7r3o;idshbh0684+04+01"); // invalid input
+echo validate("has0hg7r3o;idshbh0684+04+01"); // valid input
 echo "<br>";
 
 //Connor's code
