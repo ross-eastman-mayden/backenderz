39d8df47535867b2e63ac553b9d15cf904ade581
charliecog <charlie.coggans@mayden.co.uk>
2015-10-27 12:38:44 +0000

    updated if statement

--- tues.php
+++ tues.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$startingWord = "skdjflsjs33klj23+kjl";
+$startingWord = "s9kd jf..lsjs3(3klj23+kjl";
 
 $array = (str_split($startingWord));
 
@@ -9,7 +9,7 @@ $newWord = array();
 
 foreach ($array as &$value) {
 
-    if((ord($value)) <= 65) {
+    if((ord($value)) < 65 && (ord($value)) >= 47 || (ord($value)) == 43 ) {
         array_push($newWord, $value);
     }
 }
