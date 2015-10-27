391f4a16619bd7c1e1d92f11c2c3beb410e8bda2
charliecog <charlie.coggans@mayden.co.uk>
2015-10-27 15:04:09 +0000

    cmd alt l

--- tues.php
+++ tues.php
@@ -7,23 +7,22 @@ $array = (str_split($startingWord));
 
 $newWord = array();
 
-    foreach ($array as &$value) {
+foreach ($array as &$value) {
 
-        if((ord($value)) <= ord("9") && (ord($value)) >= ord("0") || (ord($value)) == ord("+") ) {
-            array_push($newWord, $value);
-        }
+    if ((ord($value)) <= ord("9") && (ord($value)) >= ord("0") || (ord($value)) == ord("+")) {
+        array_push($newWord, $value);
     }
+}
 
-$implodedWord = implode(" ",$newWord);
+$implodedWord = implode(" ", $newWord);
 
 $finishedWord = str_replace(' ', '', $implodedWord);
 
-    if (strlen($finishedWord) == 11 && (substr($finishedWord, 0, 2)) == '07' ) {
-        echo "Phone number validated: " . $finishedWord;
-    }
-    else {
-        echo "you shall not pass!";
-    }
+if (strlen($finishedWord) == 11 && (substr($finishedWord, 0, 2)) == '07') {
+    echo "Phone number validated: " . $finishedWord;
+} else {
+    echo "you shall not pass!";
+}
 
 ?>
 
