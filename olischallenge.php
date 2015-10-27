9c9136ffac4fb749d289357e19b605b2bfbfca8e
Ross E <ross.eastman@mayden.co.uk>
2015-10-27 12:02:57 +0000

    use of preg_exp

--- olischallenge.php
+++ olischallenge.php
@@ -1,3 +1,21 @@
 <?php
 
+    //input string of characters
+    //remove any non-numeric characters
+    //ignore "+"
+    //output error if any non-numerics are found and DIE
+    //return string
 
+    //regex??
+    //while loops??
+
+
+    function strip($string)
+        {
+            return preg_replace('/[^0-9\+]/', NULL, $string); // Removes special chars.
+        }
+
+
+    $string = "khjhdfgkjhdfg08754678936\';][+";
+
+    echo strip($string);
